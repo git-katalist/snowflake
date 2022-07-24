9a82ab39968eae28cfc8aeb587fd56ad9f8a9d01
tsots <mosalatsotetsi@gmail.com>
2022-07-24 16:01:57 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const debug = process.env.NODE_ENV !== "production";
 
-module.exports = nextConfig
+ module.exports = {
+   // basePath: '/crisp',
+   basePath: !debug ? "/snowflake" : "",
+ };
