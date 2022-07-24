235bd10b5a5a70651651ab41a8dcdfba5eeb1cf4
tsots <mosalatsotetsi@gmail.com>
2022-07-24 16:37:27 +0200

    meta test commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,13 @@ import { ThemeProvider } from "styled-components";
 
 import { Theme } from "../styles/Theme";
 import { Global } from "styles/Global";
+import Meta from "@Components/Meta";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={Theme}>
       <Global />
+      <Meta />
       <Component {...pageProps} />
     </ThemeProvider>
   );
